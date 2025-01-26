c1173aaefe272843eba376469f3fe91b2ade747d
anhtuan <anhtuan220903@gmail.com>
2025-01-26 23:55:44 +0700

    Xay dung layouts chung

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
 
@@ -10,7 +11,8 @@ database.connect();
 const app = express();
 const port = process.env.PORT;
 
-
+// App local Variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
