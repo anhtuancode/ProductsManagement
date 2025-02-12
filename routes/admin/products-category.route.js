04a7d76e23d52f26910416d2b1e98f4006396e46
anhtuan <anhtuan220903@gmail.com>
2025-02-12 15:47:12 +0700

    Tạo danh mục sản phẩm(phần 1)

---
+++ routes/admin/products-category.route.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();    
+const multer  = require('multer');
+
+const upload = multer();
+
+const controller = require("../../controllers/admin/products-category.controller");
+
+const validate = require("../../validates/admin/products-category.validate");
+
+const uploadCloud = require("../../middleware/admin/uploadCloud.middleware");
+
+router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+    "/create", 
+    upload.single('thumbnail'),
+    uploadCloud.upload,
+    // validate.createPost,
+    controller.createPost
+);
+
+module.exports = router;
