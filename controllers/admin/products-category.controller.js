b4ad23302e10d236a097d4d4138143c92eabccd3
anhtuan <anhtuan220903@gmail.com>
2025-02-14 11:27:08 +0700

    Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/products-category.controller.js
+++ controllers/admin/products-category.controller.js
@@ -48,3 +48,41 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 };
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {  
+  try {
+    const id = req.params.id;
+
+    const find = {
+      _id: id,
+      deleted: false
+    };
+    const data = await ProductCategory.findOne(find);
+  
+    const records = await ProductCategory.find({
+      deleted:false
+    });
+  
+    const newRecords = createTreeHelper.tree(records);
+    res.render("admin/pages/products-category/edit.pug", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords
+    }); 
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`)
+  }
+};
+
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {  
+  const id = req.params.id;
+  
+  req.body.position = parseInt(req.body.position);
+
+  await ProductCategory.updateOne({_id:id},req.body)
+
+  res.redirect("back");
+};
