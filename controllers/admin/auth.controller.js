8a7af2c4b9e36c449d3da49f5667c8bbb40ad9f5
anhtuan <anhtuan220903@gmail.com>
2025-02-19 16:18:59 +0700

    Làm tính năng đăng xuất

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -41,3 +41,9 @@ module.exports.loginPost = async (req, res) => {
   res.cookie("token", user.token);
   res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
 };
+
+module.exports.logout = async (req, res) => {  
+  //delete token in side cookie
+  res.clearCookie("token");
+  res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+};
