46325b02f0998b2e5725b97856704a4cd5551200
anhtuan <anhtuan220903@gmail.com>
2025-02-19 10:42:57 +0700

    Làm trang sửa tài khoản

--- controllers/admin/accounts.controller.js
+++ controllers/admin/accounts.controller.js
@@ -23,6 +23,7 @@ module.exports.index = async(req, res)=>{
         records: records
     });
 }
+
 // [GET] /admin/accounts/create
 module.exports.create = async(req, res)=>{
     const roles = await Role.find({
@@ -34,6 +35,7 @@ module.exports.create = async(req, res)=>{
         roles: roles
     });
 }
+
 // [POST] /admin/accounts/create
 module.exports.createPost = async(req, res)=>{
     const emailExist = await Account.findOne({
@@ -59,4 +61,52 @@ module.exports.createPost = async(req, res)=>{
         await record.save();
         res.redirect(`${systemConfig.prefixAdmin}/accounts`);
     }
+}
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async(req, res)=>{
+    let find = {
+        _id: req.params.id,
+        deleted: false
+    }
+
+    try {
+        const data = await Account.findOne(find);
+
+        const roles = await Role.find({
+            deleted: false
+        });
+
+        res.render("admin/pages/accounts/edit",{
+            pageTitle: "Trang chỉnh sửa tài khoản",
+            roles: roles,
+            data: data
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+    }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async(req, res)=>{
+    const id = req.params.id;
+
+    const emailExist = await Account.findOne({
+        _id: {$ne: id},
+        email: req.body.email,
+        deleted: false
+    });
+
+    if(emailExist){
+        req.flash("error", `Email ${req.body.email} này đã tồn tại`);
+    }else{
+        if(req.body.password){
+            req.body.password = md5(req.body.password);
+        }else{
+            delete req.body.password;
+        }
+        await Account.updateOne({_id: id}, req.body);
+        console.log(req.flash("success", `Cập nhật tài khoản thành công`));
+    }
+    res.redirect("back");
 }
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -12,4 +12,13 @@ router.get("/", controller.index);
 
 router.get("/create", controller.create);
 router.post("/create", upload.single('avatar'), validate.createPost, uploadCloud.upload, controller.createPost);
+
+router.get("/edit/:id", controller.edit);
+
+router.patch("/edit/:id", upload.single('avatar'), validate.editPatch, uploadCloud.upload, controller.editPatch);
+
+
+
+
+
 module.exports = router;
